5d6cacd719992b5ddf47c169b194b54027380bb2
galchi0325 <king13579u@naver.com>
2025-05-22 01:29:51 +0900

    update gene.gene.py

--- 컴프입/gene.py
+++ 컴프입/gene.py
@@ -12,53 +12,28 @@ def find_proper_genes(S,E):
 	for start in S:
 		pos = 0
 		while True:
-			start_idx = genome.find(start,pos)
+			start_idx = genome.find(start, pos)
 			if start_idx == -1:
 				break
-			end_idx = genome.find(end,pos+len(start))
+			for end in E:
+				end_idx = genome.find(end, start_idx + len(start))
+				if end_idx == -1:
+					continue 
+				candidate = genome[start_idx:end_idx + len(end)]
+				if end_idx < start_idx + len(start):
+					continue
+				inner = candidate[len(start):-len(end)]
+				if all(p not in inner for p in S + E):
+					gene_panals.append(candidate)
+			pos = start_idx + 1  
 
-
-
-		for end in E:
-			S_int = len(start)
-			E_int = len(end)
-			result = []
-			for i in range(gene_list_count):
-				Genes =gene_list[i:i+S_int]
-				result.append("".join(Genes))
-			
-			gene_string = ''
-			for j in result:
-				J = list(j)
-				if j==start:
-					if gene_string == '':
-						gene_string = j
-					else:
-						gene_string = ''
-						
-				else:
-					gene_string_1st = ''.join(list(gene_string)[0:S_int])
-					gene_string_last = ''.join(list(gene_string)[-E_int:])
-					gene_string_length = len(gene_string)
-					if gene_string_last == end and gene_string_1st == start and gene_string_length >= S_int+E_int:
-						gene_panals.append(gene_string)
-						gene_string = ''	
-					elif gene_string_1st == start:
-						gene_string+=J[3]
-	all = S+E
-	for pattern in all:
-		for panal in gene_panals:
-			panal_middle = ''.join(list(panal)[1:-1])
-			if pattern in panal_middle:
-				if panal in gene_panals:
-					gene_panals.remove(panal)
 	return(gene_panals)
 
 def find_shortest_panal(L):
 	if L == []:
 		return('None')
 	else:
-		shortest_panal = sorted(L)[0]
+		shortest_panal = min(L,key = lambda x : (len(x),x))
 		return(shortest_panal)
 
 S_i,E_i = start_end_input()
