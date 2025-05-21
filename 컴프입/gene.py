fddf5bde8831d610b3f65ef4cc640ba4c7e00851
galchi0325 <king13579u@naver.com>
2025-05-21 15:12:38 +0900

    Update gene.py

--- 컴프입/gene.py
+++ 컴프입/gene.py
@@ -3,7 +3,6 @@ def start_end_input():
 	E_i = list(input().split())
 	return(S_i,E_i)
 	
-print(start_end_input())
 
 genome = """ATAGTGTCTTCAAAGCTCCAGGTGGTCCTTCTAAATAGCGCCTAAGCAGCTTCCTTATGACAGGGTGTCGGCGATTATTTATCTCTGTGTTCTATTTTTAAGCAAGTTGTCATCATTGAAATAGAGAACTCATTTTTTTGTTTTTTGTATTTCTTCTTTTTTTTGTATGCATTTTTATTTGTGAACTGTAAATTTTCATAACGTTCACAGAGTTTTAAATAATTTACAGTTAAATCAAAATCACTTCACTACATATTGCTACTTCAAATAACTAATCTAACTGGAGTAGTGGTGTTAAACAGGAATCACGAGGCGAAGAAATCCAGTACGTGTAAACGAGCGAACGAATGAGATTATCAGTTATATTTTCATCGTCATTGTCGCGTGGACAATACACTAAACATATCGCTAAAGCTTCATCTACACGATTAGCGAAAATTTTATATGGAATTGACACCTCGCACGCAGATTTCTGCATACGATTGCACATTGTGAACAAATATGCTATTCGTGTATAAACGCCTTAAAACACATGGGTGGTCGAAT"""
 gene_list = list(genome)
@@ -13,24 +12,22 @@ result = []
 for i in range(gene_list_count):
 	Genes =gene_list[i:i+4]
 	result.append("".join(Genes))
-print(result)
 #스타트 마커와 같은 유전자 개수 찾기 까지는 성공
 i=0
 gene_panals = []
 gene_string = ''
 for j in result:
 	J = list(j)
-	if j!='GTGT':
+	if j=='GTGT':
 		if gene_string == '':
-			gene_string+=j
+			gene_string = j
 		else:
-			gene_string+=J[3]
-	else:
-		if gene_string == '':
-			gene_string+=j
-		else:
-			gene_string+=J[3]
 			gene_panals.append(gene_string)
 			gene_string = j
+	else:
+		gene_srting_1st = ''.join(list(gene_string)[0:4])
+		if gene_srting_1st == 'GTGT':
+			gene_string+=J[3]
+			
 print(gene_panals)
 		
